b9a08bce7773871eed637102676dcd416a0b7cf8
Bartoszze <112906615+Bartoszze@users.noreply.github.com>
2024-03-15 18:04:17 +0100

    Save

--- src/Components/Categories/index.js
+++ src/Components/Categories/index.js
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom";
 import errorCover from "../../Assets/Images/404.svg";
 import "./index.sass";
+import { useState } from "react";
 
 const Categories = () => {
+  const [show, setShow] = useState(false);
+
   const localStorageCategories = JSON.parse(localStorage.getItem("categories"));
+
+  if (!localStorageCategories[0]) {
+    console.log("asd");
+    // Wsywietlanie informacji zeby dodac kategorie
+  }
   const onImageError = (e) => {
     e.target.src = errorCover;
   };
